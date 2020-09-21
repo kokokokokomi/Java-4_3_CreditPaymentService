2028819635e91554f09e3f6d50fb2b8c45024e22
komisuomi <70074580+komisuomi@users.noreply.github.com>
2020-09-21 11:21:30 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,11 @@
 public class CreditPaymentService {
     public double calculate(int creditBody, int creditTerm, double percentYear) {
+
         double percentMonth = percentYear / 12 / 100;
         double generalRate = 1 + percentMonth;
-        double generalRateYear = Math.pow(generalRate, 12);
+        double generalRateYear = Math.pow(generalRate, creditTerm);
         double creditPayment = creditBody * percentMonth * generalRateYear / (generalRateYear - 1);
 
         return creditPayment;
-        }
+    }
 }
